2e8622bafd3571719adbf9d92d33fa61ec6057e3
Renier Trenuela <rtrenuela@objectbright.com>
2020-12-27 12:28:49 +0800

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::query()->insert([
+            'name'              => 'Administrator',
+            'email'             => 'admin@tabang.com',
+            'role'              => 1,
+            'email_verified_at' => now(),
+            'agency_code'       => Str::random(5),
+            'password'          => 'tabangpass', // password
+            'remember_token'    => Str::random(10),
+        ]);
+
         \App\Models\User::factory(10)->create();
         \App\Models\Employer::factory(100)->create();
         \App\Models\Employee::factory(1000)->create();
