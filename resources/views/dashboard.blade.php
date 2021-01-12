eb32382832999ddfd1b0705fb7b97e3f4b818657
Renier Trenuela <rtrenuela@objectbright.com>
2021-01-12 16:29:02 +0800

    Alert display

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,19 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
+                @canany(['admin', 'agency'])
+                    <div class="grid grid-cols-3 gap-4 p-5">
+                        <div class="col-span-3 shadow-lg">
+                            <div class="animate-pulse text-2xl text-center p-4 bg-yellow-300 border-b border-gray-200">
+                                <i class="fas fa-exclamation-triangle text-red-600"></i> 22 Overdue of Reports Detected
+                            </div>
+                        </div>
+                    </div>
+                @else
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        You're logged in!
+                    </div>
+                @endcan
             </div>
         </div>
     </div>
