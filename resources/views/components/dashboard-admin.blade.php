841442bd31c024549821f0c0ad87d978e4d4c8d6
Renier Trenuela <rtrenuela@objectbright.com>
2021-01-18 14:19:09 +0800

    check display

--- resources/views/components/dashboard-admin.blade.php
+++ resources/views/components/dashboard-admin.blade.php
@@ -15,38 +15,38 @@
             </div>
         </div>
     </div>
-    <div class="grid grid-cols-4 gap-4 p-5">
-        <div class="col-span-2 md:col-span-1 p-2 rounded shadow text-gray-600 bg-yellow-300">
-            <div class="font-merriweather mb-1 text-center md:text-5xl">
+@endif
+<div class="grid grid-cols-4 gap-4 p-5">
+    <div class="col-span-2 md:col-span-1 p-2 rounded shadow text-gray-600 bg-yellow-300">
+        <div class="font-merriweather mb-1 text-center md:text-5xl">
                 <span class="bg-gray-200 text-center rounded-full p-5">
                     {{ \App\Models\User::query()->where('role','2')->count() }}
                 </span>
-            </div>
-            <div class="md:text-2xl font-semibold mt-6">Agencies</div>
         </div>
-        <div class="col-span-2 md:col-span-1 p-2 rounded shadow text-gray-600 bg-green-300">
-            <div class="font-merriweather mb-1 text-center md:text-5xl">
+        <div class="md:text-2xl font-semibold mt-6">Agencies</div>
+    </div>
+    <div class="col-span-2 md:col-span-1 p-2 rounded shadow text-gray-600 bg-green-300">
+        <div class="font-merriweather mb-1 text-center md:text-5xl">
                 <span class="bg-gray-200 text-center rounded-full p-5">
                 {{ \App\Models\User::query()->where('role','3')->count() }}
                 </span>
-            </div>
-            <div class="md:text-2xl font-semibold mt-6">Employer</div>
         </div>
-        <div class="col-span-2 md:col-span-1 p-2 rounded shadow text-gray-600 bg-pink-300">
-            <div class="font-merriweather mb-1 text-center md:text-5xl">
+        <div class="md:text-2xl font-semibold mt-6">Employer</div>
+    </div>
+    <div class="col-span-2 md:col-span-1 p-2 rounded shadow text-gray-600 bg-pink-300">
+        <div class="font-merriweather mb-1 text-center md:text-5xl">
                 <span class="bg-gray-200 text-center rounded-full p-5">
                 {{ \App\Models\Candidate::query()->count() }}
                 </span>
-            </div>
-            <div class="md:text-2xl font-semibold mt-6">OFW</div>
         </div>
-        <div class="col-span-2 md:col-span-1 p-2 rounded shadow text-gray-600 bg-purple-300">
-            <div class="font-merriweather mb-1 text-center md:text-5xl">
+        <div class="md:text-2xl font-semibold mt-6">OFW</div>
+    </div>
+    <div class="col-span-2 md:col-span-1 p-2 rounded shadow text-gray-600 bg-purple-300">
+        <div class="font-merriweather mb-1 text-center md:text-5xl">
                 <span class="bg-gray-200 text-center rounded-full p-5">
                 {{ \App\Models\Candidate::query()->where('deployed', 'yes')->count() }}
                 </span>
-            </div>
-            <div class="md:text-2xl font-semibold mt-6">Deployed</div>
         </div>
+        <div class="md:text-2xl font-semibold mt-6">Deployed</div>
     </div>
-@endif
+</div>
