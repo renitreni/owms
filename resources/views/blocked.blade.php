c579acb2dd405a774fac7c56733c28b090d9444a
Renier Trenuela <rtrenuela@objectbright.com>
2021-04-27 17:07:49 +0800

    add

--- resources/views/blocked.blade.php
+++ resources/views/blocked.blade.php
@@ -6,9 +6,16 @@
     </x-slot>
 
     <div id="app" class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <p>Your Account has been temporarily Suspended.</p>
-            <p>Reason for suspension: <span class="font-bold bg-red-100 p-1">{{ $agency->status }}</span></p>
+        <div class="flex flex-col">
+            <div class="flex flex-row mx-auto">
+                <img src="{{ $agency->logo_path }}" class="m-2" width="150px" height="150px">
+                <img src="{{ asset('tabang-logo/polo-logo-new.jpg') }}" class="m-2" width="150px" height="150px">
+                <img src="{{ asset('tabang-logo/3-star.png') }}" class="m-2" width="150px" height="150px">
+            </div>
+            <div class="flex max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <p>Your Account has been temporarily Suspended.</p>
+                <p>Reason for suspension: <span class="font-bold bg-red-100 p-1">{{ $agency->status }}</span></p>
+            </div>
         </div>
     </div>
 
