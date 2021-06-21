378d67cc8b85621832d26b4bc6e30bbcc4a0a34c
Renier Trenuela <rtrenuela@objectbright.com>
2021-06-21 16:22:31 +0800

    fix

--- config/mail.php
+++ config/mail.php
@@ -38,13 +38,10 @@ return [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'encryption' => 'tls',
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-            'auth_mode' => null,
         ],
-
         'ses' => [
             'transport' => 'ses',
         ],
