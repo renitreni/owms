3ac804700c1c973fbc7a77097c1877c79e1efd1c
rtrenuela <rtrenuela@objectbright.com>
2022-02-21 10:16:32 +0800

    update

--- app/Http/Middleware/AgencyIsBlockedMiddleware.php
+++ app/Http/Middleware/AgencyIsBlockedMiddleware.php
@@ -11,15 +11,16 @@ class AgencyIsBlockedMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
         if (auth()->user()->role == '2') {
-            $agency_count = Agency::query()->where('id', auth()->user()->agency_id)->where('status', 'Active')->count();
-            if($agency_count == '0') {
+            $agency_count = Agency::query()->where('id', auth()->user()->agency_id)->where('status', '<>',
+                'Active')->count();
+            if ($agency_count == '0') {
                 return redirect()->route('blocked');
             }
         }
