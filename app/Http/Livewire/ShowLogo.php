f13244165e2bacc9e2d9e1ac60222e69b4aa33d0
rtrenuela <rtrenuela@objectbright.com>
2022-03-14 14:52:41 +0800

    update

--- app/Http/Livewire/ShowLogo.php
+++ app/Http/Livewire/ShowLogo.php
@@ -4,12 +4,19 @@ namespace App\Http\Livewire;
 
 use App\Models\Agency;
 use Livewire\Component;
+use Throwable;
 
 class ShowLogo extends Component
 {
     public function render()
     {
-        $pathLogo = Agency::query()->where('id', auth()->user()->agency_id)->first()['logo_path'];
+        try {
+        
+            $pathLogo = Agency::query()->where('id', auth()->user()->agency_id)->first()['logo_path'];
+        } catch (Throwable $e) {
+            report($e);
+            $pathLogo = '';
+        }
         
         return view('livewire.show-logo', ['pathLogo' => $pathLogo]);
     }
